741764fd890a039b1666248c759d554331f1e6ae
Khris <sr.highyitan@gmail.com>
2024-12-03 19:53:53 +0100

    'asd'

--- blog2/app/Http/Controllers/Api/PostController.php
+++ blog2/app/Http/Controllers/Api/PostController.php
@@ -14,8 +14,14 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::all();
+        // $posts = Post::paginate(3);  // crea una sortida amb paginació
+        // $posts = Post::with([])->get();  // no té sentit
+        // $posts = Post::with(["user", "category", "comments"])->get();  // post amb les taules relacionades, més óptima
+        // $posts = Post::with(["user", "category", "comments", "comments.images"])->get();
+        // $posts = Post::with(["user", "category", "comments", "comments.images"])->paginate(3);
 
-        return response()->json($posts);
+        return response()->json($posts);  // --> torna una resposta serialitzada en format 'json'
+        // return (PostResource::collection($posts))->additional(['meta' => 'Posts mostrats correctament']);  // torna una resposta personalitzada
     }
 
     /**
@@ -29,9 +35,20 @@ class PostController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Post $post)
     {
-        //
+        // Post::find($post->id);  // no cal fer-ho, el model 'Post' ja ho fa de manera implícita
+       
+        // amb 'load()'
+        // $post->load('user')->load('category')->load('comments')->load('comments.images');
+
+        // amb 'with()'
+        // $newPost = Post::with(["user","category","comments","comments.images"])->find($post->id);
+        // return response()->json($newPost);
+        // return (new PostResource($newPost))->additional(['meta' => 'Post mostrat correctament']);
+       
+        return response()->json($post);
+        // return (new PostResource($post))->additional(['meta' => 'Post mostrat correctament']);
     }
 
     /**
